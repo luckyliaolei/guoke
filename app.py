71b8b98a5f3cf615e14fb53f05491a9428e923e8
liaolei <397635042@qq.com>
2019-08-07 00:59:58 +0800

    add db

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from db import users
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return render_template('index.html')
+    if request.method == 'POST':
+        users.insert_one(dict(request.form))
+        return 'success'
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
